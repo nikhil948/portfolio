d9c0bc70d5980d7f3ff925fbc5d6780647481a2e
nikhil948 <nikhildubey948@gmail.com>
2026-03-28 23:40:44 +0530

    Split chunks for faster loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ["three"],
+          fiber: ["@react-three/fiber", "@react-three/drei"],
+          rapier: ["@react-three/rapier"],
+          postprocessing: ["@react-three/postprocessing"],
+        },
+      },
+    },
+  },
 });
