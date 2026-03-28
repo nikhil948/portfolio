ba09994a9798017c12ba6319653f14a495d2369c
nikhil948 <nikhildubey948@gmail.com>
2026-03-28 22:49:59 +0530

    Fix gsap-trial type declarations for SplitText and ScrollSmoother

--- src/types/gsap-trial.d.ts
+++ src/types/gsap-trial.d.ts
@@ -1,4 +1,20 @@
 declare module 'gsap-trial/SplitText' {
-  const SplitText: any;
+  class SplitText {
+    chars: Element[];
+    words: Element[];
+    lines: Element[];
+    constructor(target: any, vars?: any);
+    revert(): void;
+  }
+  export { SplitText };
   export default SplitText;
+}
+
+declare module 'gsap-trial/ScrollSmoother' {
+  class ScrollSmoother {
+    static create(vars?: any): ScrollSmoother;
+    paused(value?: boolean): ScrollSmoother;
+  }
+  export { ScrollSmoother };
+  export default ScrollSmoother;
 }
